9d498d3d6ca8f4fd85e37be46fe2f901065ef4a1
David Heap <davidheap.DH@hotmail.co.uk>
2022-03-03 12:24:42 +0000

    changed a few things

---
+++ Matter-2022-stub/Matter-2022-stub/js/Level3.js
@@ -0,0 +1,9 @@
+class Level3 extends BaseScene {
+      /**@type {string} */
+      static sceneID ="level3"
+      constructor(constructor){
+      super(Level3.sceneID)
+      this.tileDataKey = "level3"
+      this.tileDataSource = "assets/tiledStuff/level3.json"
+      }
+}
